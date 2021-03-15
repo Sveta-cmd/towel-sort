9838cdaa4b5546c9583483fb7d1f8465b5616677
Sveta-cmd <65214737+Sveta-cmd@users.noreply.github.com>
2021-03-16 01:40:56 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-
 // You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+    if (matrix == null)
+        return [];
+    for (i = 1; i < matrix.length; i = i + 2) {
+        matrix[i] = matrix[i].reverse();
+    }
+    return matrix.flat();
 }
